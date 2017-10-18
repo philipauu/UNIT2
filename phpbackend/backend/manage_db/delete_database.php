f24b4c0b2cc52638cd1bd352b05b37e9dcdd3412
philipauu <Phil@Phils-MacBook.local>
2017-10-18 14:48:15 +0800

    I would gladly raise my voice in praise of women, only they won't let me raise my voice. 		-- Winkle

--- phpbackend/backend/manage_db/delete_database.php
+++ phpbackend/backend/manage_db/delete_database.php
@@ -6,16 +6,29 @@ $db = new DbConnect($admin, $pass);
 
 $db_name = $_GET['db_name'];
 
-$sql = "DROP DATABASE $db_name";
+function safety ($db_name){
+  $restricted_names = ['information_schema', 'mysql', 'performance_schema', 'sys', 'wdi'];
+  if (in_array($db_name, $restricted_names)){
+    logger('if statement is true');
+    echo "$db_name can't be deleted!";
 
-$result = $db->conn->query($sql);
+  } else {
 
-if($result){
-  logger("dropped database");
-  echo "dropped database $db_name";
-  // do true stuff
-}
-  else{
-    logger($db->conn->error);
-    // do false stuff
+    logger('if statemnet is false');
+    global $db;
+    $sql = "DROP DATABASE $db_name";
+    $result = $db->conn->query($sql);
+
+    if($result){
+      logger("dropped database");
+      echo "dropped database $db_name";
+      // do true stuff
+    }
+      else{
+        logger($db->conn->error);
+        // do false stuff
+    }
   }
+}
+
+safety($db_name);
--- phpbackend/backend/manage_users/delete_users.php
+++ phpbackend/backend/manage_users/delete_users.php
@@ -6,16 +6,29 @@ $user = new DbConnect($admin, $pass);
 
 $user_name = $_GET['user_name'];
 
-$sql = "DROP USERS $user_name";
+function safety ($user_name){
+  $restricted_names = ['mysql.session', 'mysql.sys', 'root'];
+  if (in_array($user_name, $restricted_names)){
+    logger('if statement is true');
+    echo "$user_name can't be deleted!";
 
-$result = $user->conn->query($sql);
+  } else {
 
-if($result){
-  logger("dropped user");
-  echo "dropped user $user_name";
-  // do true stuff
-}
-  else{
-    logger($user->conn->error);
-    // do false stuff
+    logger('if statemnet is false');
+    global $user;
+    $sql = "DROP USER $user_name";
+    $result = $user->conn->query($sql);
+
+    if($result){
+      logger("dropped user");
+      echo "dropped user $user_name";
+      // do true stuff
+    }
+      else{
+        logger($user->conn->error);
+        // do false stuff
+    }
   }
+}
+
+safety($user_name);
